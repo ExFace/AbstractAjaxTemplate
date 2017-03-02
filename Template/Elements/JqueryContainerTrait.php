0c0967d925283b7fa0f8bb1d1248844fd6dc5e0d
Andrej Kabachnik <andrej_ka@gmx.de>
2017-03-02 12:24:41 +0100

    FIX data getters for containers with attributes of multiple objects

--- Template/Elements/JqueryContainerTrait.php
+++ Template/Elements/JqueryContainerTrait.php
@@ -49,19 +49,18 @@ trait JqueryContainerTrait {
 		/* @var $widget \exface\Core\Widgets\Container */
 		$widget = $this->get_widget();
 		$data_getters = array();
+		// Collect JS data objects from all inputs in the container
 		foreach ($widget->get_input_widgets() as $child){
-			// FIXME $child->get_meta_object()->is_exactly($action->get_meta_object()) makes it impossible to use widgets with custom objects in forms!
 			if (!$child->implements_interface('iSupportStagedWriting')){
-				// Collect data from all widgets that take regular input (all sorts of input fields, etc.). Make sure, only data directly related
-				// to the object the action is performed upon is collected
-				//$output_rows .= $js_data_variable . '.rows[0]["' . $child->get_attribute_alias() . '"] = ' . $this->get_template()->get_element($child)->build_js_value_getter() . ";\n";
 				$data_getters[] = $this->get_template()->get_element($child)->build_js_data_getter($action);
 			} else {
 				// TODO get data from non-input widgets, that support deferred CRUD operations staging their data in the GUI	
 			}
 		}
 		if (count($data_getters) > 0){
-			return "$.extend(true, {},\n" . implode(",\n", $data_getters) . "\n)";
+			// Merge all the JS data objects, but remember to overwrite the head oId in the resulting object with the object id 
+			// of the container itself at the end! Otherwise the object id of the last widget in the container would win!
+			return "$.extend(true, {},\n" . implode(",\n", $data_getters) . ",\n{oId: '" . $widget->get_meta_object_id() . "'}\n)";
 		} else {
 			return '{}';
 		}

fd256886be3d3650a244ae1b89622b4f1eda9a1b
Andrej Kabachnik <andrej_ka@gmx.de>
2017-04-27 12:35:56 +0200

    FIX compatibility with new widget id space feature

--- Template/Elements/JqueryLiveReferenceTrait.php
+++ Template/Elements/JqueryLiveReferenceTrait.php
@@ -4,8 +4,7 @@ trait JqueryLiveReferenceTrait {
 	
 	protected function build_js_live_reference(){
 		$output = '';
-		if ($this->get_widget()->get_value_expression() && $this->get_widget()->get_value_expression()->is_reference()){
-			$link = $this->get_widget()->get_value_expression()->get_widget_link();
+		if ($link = $this->get_widget()->get_value_widget_link()){
 			$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
 			if ($linked_element){
 				$output = '
@@ -28,8 +27,7 @@ trait JqueryLiveReferenceTrait {
 	
 	public function get_linked_template_element(){
 		$linked_element = null;
-		if ($this->get_widget()->get_value_expression() && $this->get_widget()->get_value_expression()->is_reference()){
-			$link = $this->get_widget()->get_value_expression()->get_widget_link();
+		if ($link = $this->get_widget()->get_value_widget_link()){
 			$linked_element = $this->get_template()->get_element_by_widget_id($link->get_widget_id(), $this->get_page_id());
 		}
 		return $linked_element;

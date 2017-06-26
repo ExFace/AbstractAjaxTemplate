6377d86be67631333ccedd2beae68212f3416d18
Stefan Leupold <stefan.leupold@salt-solutions.de>
2017-06-26 17:08:18 +0200

    #NEW LayoutInterface und LayoutTrait hinzugefuegt

---
+++ Template/Elements/JqueryLayoutTrait.php
@@ -0,0 +1,15 @@
+<?php
+namespace exface\AbstractAjaxTemplate\Template\Elements;
+
+trait JqueryLayoutTrait {
+
+    /**
+     * Returns an inline JavaScript-Snippet to start the layouting of the widget.
+     *
+     * @return string
+     */
+    public function buildJsLayouter()
+    {
+        return $this->getId() . '_layouter()';
+    }
+}
